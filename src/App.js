bb18be65ee2c0ed84e22527d4c8595c0d2ea720c
Hasib787 <mr.hasib45@gmail.com>
2021-02-23 19:52:14 +0600

    some changed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name: "Hasib",
+    age: 25
+  }
+  const person2 = {
+    name: "Surovi",
+    age: 22
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +17,9 @@ function App() {
         <p>
           Edit Done <code>src/App.js</code> and save to reload.
         </p>
-       
+        <h2>Husband: {person.name + " " + person.age}</h2>
+        <h2>Wife: {person2.name + " " + person2.age}</h2>
+        <p>My first React Paragraph</p>
       </header>
     </div>
   );
