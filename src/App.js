6e56a8c3d84c61a315e65c0f880a04f31a366438
Hasib787 <mr.hasib45@gmail.com>
2021-02-23 20:55:12 +0600

    added some changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
     name: "Surovi",
     age: 22
   }
+  let style={
+    backgroundColor:'cyan',
+    color: 'black'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +21,8 @@ function App() {
         <p>
           Edit Done <code>src/App.js</code> and save to reload.
         </p>
-        <h2>Husband: {person.name + " " + person.age}</h2>
-        <h2>Wife: {person2.name + " " + person2.age}</h2>
+        <h2 style={style}>Husband: {person.name + " " + person.age}</h2>
+        <h2 style={{backgroundColor:'pink', color:'black'}}>Wife: {person2.name + " " + person2.age}</h2>
         <p>My first React Paragraph</p>
       </header>
     </div>
