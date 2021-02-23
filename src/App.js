c6b9ee909d278edc20945a583cc6b73aa3dce426
Hasib787 <mr.hasib45@gmail.com>
2021-02-24 03:28:39 +0600

    added a product item section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products=[
+    {name:'Photoshop', price:'$99.99'},
+    {name:'Illustrator', price:'$60.99'},
+    {name:'PDF Reader', price:'$6.70'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
+        
         <p>I'm a React person</p>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
         <Person name="Hasib" job="Toto Company manager"></Person>
         <Person name="Md. Masud Rana" job="Software Engineer"></Person>
       </header>
     </div>
   );
+}
+
+function Product(props){
+  const productStyle= {
+    border:'1px solid gray',
+    borderRadius:'7px',
+    color:'blue',
+    backgroundColor:'lightgray',
+    height:'300px',
+    width:'300px',
+    float:'left',
+    margin:'10px'
+  }
+  return (
+    <div style={productStyle}>
+      <h3>{props.product.name}</h3>
+      <h5>{props.product.price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
 }
 
 function Person(props) {
