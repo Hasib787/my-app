1ae5395e5967f910445265da6013973958f5844d
Hasib787 <mr.hasib45@gmail.com>
2021-02-24 00:44:43 +0600

    added some style

--- src/App.js
+++ src/App.js
@@ -2,31 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const person = {
-    name: "Hasib",
-    age: 25
-  }
-  const person2 = {
-    name: "Surovi",
-    age: 22
-  }
-  let style={
-    backgroundColor:'cyan',
-    color: 'black'
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <h2 style={style}>Husband: {person.name + " " + person.age}</h2>
-        <h2 style={{backgroundColor:'pink', color:'black'}}>Wife: {person2.name + " " + person2.age}</h2>
-        <p>My first React Paragraph</p>
+        <p>I'm a React person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  const personStyle={
+    border:'2px solid yellow',
+    margin: '.8em'
+  }
+  return (
+    <div style = {personStyle}>
+      <h1>Name: Hasibul Hasan</h1>
+      <h2>Web Developer</h2>
+    </div>
+  )
 }
 
 export default App;
