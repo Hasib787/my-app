d77f82eff81ee70e252d21dc52284c36be3a2dd3
Hasib787 <mr.hasib45@gmail.com>
2021-02-24 22:02:10 +0600

    added increase handler

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const studentNames=['Hridoy', 'Riad', 'Rishad', 'Rakib', 'Hasib','Zara'];
-  const products=[
-    {name:'Photoshop', price:'$99.99'},
-    {name:'Illustrator', price:'$60.99'},
-    {name:'PDF Reader', price:'$6.70'},
-    {name:'Windows Pro', price:'$199.99'}
+  const studentNames = ['Hridoy', 'Riad', 'Rishad', 'Rakib', 'Hasib', 'Zara'];
+  const products = [
+    { name: 'Photoshop', price: '$99.99' },
+    { name: 'Illustrator', price: '$60.99' },
+    { name: 'PDF Reader', price: '$6.70' },
+    { name: 'Windows Pro', price: '$199.99' }
   ]
 
 
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>I'm a React person</p>
         <Counter></Counter>
         <ul>
           {
-            studentNames.map(student=><li>{student}</li>)
+            studentNames.map(student => <li>{student}</li>)
           }
         </ul>
         {
-        products.map(pd=><Product product={pd}></Product>)
+          products.map(pd => <Product product={pd}></Product>)
         }
         <Person name="Hasib" job="Toto Company manager"></Person>
         <Person name="Md. Masud Rana" job="Software Engineer"></Person>
@@ -33,50 +33,52 @@ function App() {
   );
 }
 
-function Product(props){
-  const productStyle= {
-    border:'1px solid gray',
-    borderRadius:'7px',
-    color:'blue',
-    backgroundColor:'lightgray',
-    height:'300px',
-    width:'300px',
-    float:'left',
-    margin:'10px',
+function Product(props) {
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '7px',
+    color: 'blue',
+    backgroundColor: 'lightgray',
+    height: '300px',
+    width: '300px',
+    float: 'left',
+    margin: '10px',
   }
-  const {name, price} = props.product;
+  const { name, price } = props.product;
 
   return (
     <div style={productStyle}>
       <h3>{name}</h3>
       <h5>{price}</h5>
-      <button style={{backgroundColor:'green', color:'white', borderRadius:'5px'}}>Buy now</button>
+      <button style={{ backgroundColor: 'green', color: 'white', borderRadius: '5px' }}>Buy now</button>
     </div>
   )
 }
 
 //counter
-function Counter(){
-  const [count, setCount] = useState(10);
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () =>setCount(count + 1);
   return (
     <div>
       <h1>Count:{count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   )
-}
-
-function Person(props) {
-  const personStyle = {
-    border: '2px solid yellow',
-    margin: '.8em',
-    width: '600px'
   }
-  return (
-    <div style={personStyle}>
-      <h1>Name: {props.name} </h1>
-      <h3>Profession: {props.job} </h3>
-    </div>
-  )
-}
 
-export default App;
+  function Person(props) {
+    const personStyle = {
+      border: '2px solid yellow',
+      margin: '.8em',
+      width: '600px'
+    }
+    return (
+      <div style={personStyle}>
+        <h1>Name: {props.name} </h1>
+        <h3>Profession: {props.job} </h3>
+      </div>
+    )
+  }
+
+  export default App;
